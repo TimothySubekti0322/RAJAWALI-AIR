a13c176887928eb555f7e3b844929c8cb4434608
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-01-12 16:10:27 +0700

    commit untuk carousel dan the best

---
+++ src/components/Client/TheBest/TheBest.tsx
@@ -0,0 +1,108 @@
+const TheBest = () => {
+  return (
+    <>
+      <section className="">
+        <div className="container grid gap-4 grid-cols-2 grid-rows-3 mt-[8] pt-[4.44rem] sm:container sm:mx-auto sm:items-center sm:grid sm:grid-cols-1 md:grid-cols-2">
+          <div className="container sm:mx-auto">
+            <img src="/src/assets/pramugari.png" alt="" className="sm:w-full sm:h-auto" />
+          </div>
+          <div className="mr-[5.06rem] sm:items-center ">
+            <div className="justify-end w-full text-right">
+              <span className="text-neutral-600 text-3xl font-bold font-['Open Sans'] leading-10">
+                We{" "}
+              </span>
+              <span className="text-blue-500 text-3xl font-bold font-['Open Sans'] leading-10">
+                Provide
+              </span>
+              <span className="text-neutral-600 text-3xl font-bold font-['Open Sans'] leading-10">
+                {" "}
+                The Best
+              </span>
+            </div>
+            <div className="grid justify-end w-full font-['Open Sans'] mt-[0.62rem]">
+              Embark on a journey with our budget-friendly flight to a favorite
+              destination
+            </div>
+
+            <div className="flex mt-[2.94rem]">
+              <div className="flex-initial mr-[1.06rem] w-11 h-11 p-2 bg-blue-500 rounded-full justify-center items-center inline-flex mt-4">
+                <img src="/src/assets/search.png" alt="" />
+              </div>
+              <div className="flex-1 w-32">
+                <p className="w-96 h-6 text-neutral-600 text-base font-bold font-['Open Sans'] leading-snug text-justify">
+                  Search and Compare
+                </p>
+                <p>
+                  Allows you to search for flights by various parameters like
+                  departure and arrival cities, dates, number of passengers, etc
+                </p>
+              </div>
+            </div>
+
+            <div className="flex mt-7">
+              <div className="flex-initial mr-[1.06rem] w-11 h-11 p-2 bg-blue-500 rounded-full justify-center items-center inline-flex mt-4">
+                <img src="/src/assets/carbon_request-quote.png" alt="" />
+              </div>
+              <div className="flex-1 w-32 ">
+                <p className="w-96 h-6 text-neutral-600 text-base font-bold font-['Open Sans'] leading-snug text-justify">
+                  Fulfill your Request
+                </p>
+                <p>
+                  We offer online seat selection with complete information, meal
+                  selection, and also inform estimated baggage prices{" "}
+                </p>
+              </div>
+            </div>
+
+            <div className="flex mt-7">
+              <div className="flex-initial mr-[1.06rem] w-11 h-11 p-2 bg-blue-500 rounded-full justify-center items-center inline-flex mt-2">
+                <img src="/src/assets/plane.png" alt="" />
+              </div>
+              <div className="flex-1 w-32">
+                <p className="w-96 h-6 text-neutral-600 text-base font-bold font-['Open Sans'] leading-snug text-justify">
+                  Multi-city And Stopover Options
+                </p>
+                <p>
+                  Allows you to book complete routes involving multiple cities
+                  or layovers
+                </p>
+              </div>
+            </div>
+
+            <div className="flex mt-7">
+              <div className="flex-initial mr-[1.06rem] w-11 h-11 p-2 bg-blue-500 rounded-full justify-center items-center inline-flex mt-2">
+                <img src="/src/assets/phone.png" alt="" />
+              </div>
+              <div className="flex-1 w-32">
+                <p className="w-96 h-6 text-neutral-600 text-base font-bold font-['Open Sans'] leading-snug text-justify">
+                  Fast Response Call Center
+                </p>
+                <p>
+                  We have fast-response call center that will help you anytime
+                  you need (24/7 hours)
+                </p>
+              </div>
+            </div>
+
+            <div className="flex mt-7">
+              <div className="flex-initial mr-[1.06rem] w-11 h-11 p-2 bg-blue-500 rounded-full justify-center items-center inline-flex mt-2">
+                <img src="/src/assets/ion_notifications-outline.png" alt="" />
+              </div>
+              <div className="flex-1 w-32">
+                <p className="w-96 h-6 text-neutral-600 text-base font-bold font-['Open Sans'] leading-snug text-justify">
+                  Flight Tracking and Notification
+                </p>
+                <p>
+                  Keep you informed about delays, cancellations, and gate
+                  changes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TheBest;
--- src/pages/client/Landing.tsx
+++ src/pages/client/Landing.tsx
@@ -1,6 +1,12 @@
+
+import Carousel from "../../components/Client/Carousel/Carousel";
+import TheBest from "../../components/Client/TheBest/TheBest";
+
 const Landing = () => {
   return (
     <>
+    <Carousel/>
+    <TheBest/>
     </>
   );
 };
