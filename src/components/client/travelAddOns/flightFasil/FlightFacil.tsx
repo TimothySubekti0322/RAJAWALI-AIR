b21754a5473c45f72c8508eb43482d357dadb281
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-01-20 21:57:27 +0700

    selesai card fligt facil

---
+++ src/components/client/travelAddOns/flightFasil/FlightFacil.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+interface FlightFacilProps {
+    img1: string;
+    textBold: string;
+    paraf: string;
+    img2: string;
+}
+export const FlightFacil: React.FC<FlightFacilProps> = ({img1, textBold, paraf, img2}) => {
+  return (
+    <>
+      <div className="flex w-[20.5rem] h-[5.5rem] bg-white rounded-lg shadow mt-[0.625rem]">
+        <div className="p-[0.75rem]">
+          <div className="flex flex-col-2 flex-grow">
+            <div className="flex-auto w-[15.75rem]">
+            <div className="inline-flex text-black">
+              <div className="mr-[0.5rem]">
+                <img src={img1} alt="" />
+              </div>
+              <div className="font-bold font-['Roboto'] leading-tight tracking-tight">
+                <p>{textBold}</p>
+              </div>
+            </div>
+            <div className="flex-col w-[15.75rem] text-neutral-600 text-xs font-normal font-['Roboto'] leading-none text-left text-[0.625rem] mt-[0.5rem]">
+              <p>
+                {paraf}
+              </p>
+            </div>
+            </div>
+            <div className="flex text-black ml-5 my-5">
+                <img src={img2} alt="" />
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/client/TravelAddOns.tsx
+++ src/pages/client/TravelAddOns.tsx
@@ -2,24 +2,61 @@ import { FiFilter } from "react-icons/fi";
 import { MdOutlineSort } from "react-icons/md";
 import BodyLayout from "../../components/client/bodyLayout";
 import HeaderLayout from "../../components/client/headerLayout";
+import { FaArrowLeft } from "react-icons/fa";
+import { FlightFacil } from "../../components/client/travelAddOns/flightFasil/FlightFacil";
+// import ExtraProtections from "../../components/client/travelAddOns/extraProteksi/ExtraProtections";
 
 const TravelAddOns = () => {
   return (
     <section className="w-full min-h-screen bg-[#f7f7f7] relative">
       <HeaderLayout>
-        <div className="flex ">
-          <div className="flex-initial w-14 h-14">sfsdfsdfsf</div>
-          <div className="flex-initial text-center text-stone-50 text-base font-bold font-['Roboto'] tracking-tight">
+        <div className="inline-flex items-center">
+          <div className="flex-initial">
+            <button
+              onClick={() => {
+                window.location.href = "/";
+              }}
+            >
+              <FaArrowLeft className="text-xl text-white" />
+            </button>
+          </div>
+          <div className="flex-initial text-center text-stone-50 text-base font-bold font-['Roboto'] tracking-tight pl-[5.31rem]">
             <p className="text-white">Travel Add-ons</p>
           </div>
         </div>
       </HeaderLayout>
 
       <BodyLayout paddingBottomSize="5rem">
-        <div className="inline-flex pt-[1rem] px-[1rem]">
-          <p className="text-black text-base font-bold font-['Roboto'] leading-snug">
+        <div className="inline-flex pt-[1rem] px-[1rem] flex-col">
+          <p className="text-black text-base font-bold font-['Roboto'] leading-snug mb-[0.5rem]">
             Flight Facilities
           </p>
+          <FlightFacil
+            img1="/public/images/travel-add-ons/bag.png"
+            textBold="Baggage"
+            paraf="You can bring up to 20kg baggage per passenger. Are you sure this is enough?"
+            img2="/public/images/travel-add-ons/tambah.png"
+          />
+          <FlightFacil
+            img1="/public/images/travel-add-ons/seat.png"
+            textBold="Seat Number"
+            paraf="Baggage addition successfully selected.
+            Tap to view or make changes."
+            img2="/public/images/travel-add-ons/check.png"
+          />
+          <FlightFacil
+            img1="/public/images/travel-add-ons/meal.png"
+            textBold="In-flight Meals"
+            paraf="Don’t let yourself go hungry during the flight.
+            Enjoy the meals we have prepared for you."
+            img2="/public/images/travel-add-ons/tambah.png"
+          />
+
+          <p className="text-black text-base font-bold font-['Roboto'] leading-snug mb-[0.5rem] mt-[1.5rem]">
+            Extra Protections
+          </p>
+
+         {/* <ExtraProtections /> */}
         </div>
       </BodyLayout>
 
