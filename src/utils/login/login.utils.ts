54f398421977501225f31e456ab8b81aa1ac2ffb
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2024-02-08 00:16:21 +0700

    responsive login + integrate API

---
+++ src/utils/login/login.utils.ts
@@ -0,0 +1,6 @@
+export const validateEmail = (email: string): boolean => {
+  // Regular expression to validate email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
