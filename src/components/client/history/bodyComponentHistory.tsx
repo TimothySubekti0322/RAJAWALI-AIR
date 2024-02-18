6295931e3f301ab7224e2ec8dbbd6417f5eedf70
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2024-02-18 21:41:45 +0700

    fix unused variable

--- src/components/client/history/bodyComponentHistory.tsx
+++ src/components/client/history/bodyComponentHistory.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { HistoryGroup } from "../../../utils/history/history.interface";
-import HistoryCardCancel from "./historyCardCancel";
+// import HistoryCardCancel from "./historyCardCancel";
 import HistoryCardPurchase from "./historyCardPurchase";
-import HistoryCardReschedule from "./historyCardReschedule";
-import HistoryCard from "./HistoryCard";
+// import HistoryCardReschedule from "./historyCardReschedule";
+// import HistoryCard from "./HistoryCard";
 
 interface bodyComponentHistoryProps {
   historyGroup: HistoryGroup[];
@@ -21,7 +21,7 @@ const bodyComponentHistory: React.FC<bodyComponentHistoryProps> = ({
             <p className="mt-4 font-bold text-black">{history.title}</p>
             {/* Card in Month */}
             <div className="flex flex-col mt-2 space-y-4">
-              {history.date.map((date, index) =>{
+              {history.date.map((date, index) => {
                 return (
                   <div key={index}>
                     <HistoryCardPurchase />
