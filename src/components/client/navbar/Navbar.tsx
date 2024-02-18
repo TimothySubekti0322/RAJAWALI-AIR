47fd287e1848cd0cc1e9641a55b28c1c1c0bf7b2
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-02-19 01:11:48 +0700

    push

--- src/components/client/navbar/Navbar.tsx
+++ src/components/client/navbar/Navbar.tsx
@@ -8,54 +8,6 @@ import Orders from "../../../assets/orders icon.png";
 import Profile from "../../../assets/profile icon.png";
 import Inbox from "../../../assets/save icon.png";
 function Navbar() {
-<<<<<<< HEAD
-    const navigate = useNavigate();
-    return (
-        <>
-            <section>
-                <div className="flex justify-between items-center mx-[10px] pt-[30px]">
-                    <Link to="/" className="items-center flex">
-                        <img className="h-[50px] w-[71px]" src={Rajawali} alt="" />
-                        <div className="font-extrabold text-2xl text-white">RAJAWALI AIR</div>
-                    </Link>
-                    <div className="flex items-center">
-                        <Link to="/" className="flex items-center mx-3 bg-transparant hover:bg-slate-500 p-3 rounded-lg">
-                            <img className="h-[25px] w-[25px]" src={Favorite} alt="" />
-                            <div className="ml-2 text-white">Favorite</div>
-                        </Link>
-                        <Link to="/notification" className="flex items-center mx-3 bg-transparant hover:bg-slate-500 p-3 rounded-lg">
-                            <img className="h-[21px] w-[21px]" src={Inbox} alt="" />
-                            <div className="ml-2 text-white">Inbox</div>
-                        </Link>
-                        <Link to="/" className="flex items-center mx-3 bg-transparant hover:bg-slate-500 p-3 rounded-lg">
-                            <img className="h-[21px] w-[21px]" src={Orders} alt="" />
-                            <div className="ml-2 text-white">Orders</div>
-                        </Link>
-                        <div className="flex items-center mx-3 bg-transparant hover:bg-slate-500 p-3 rounded-lg">
-                            <img className="h-[21px] w-[21px]" src={Flag} alt="" />
-                            <select className="ml-2 bg-transparent text-white">
-                                <option>
-                                    ID
-                                </option>
-                                <option>
-                                    ENG
-                                </option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="mx-3 text-white"><b>Rajawali</b> Member</div>
-                        <div className="items-center flex">
-                            <img className="h-[25px] w-[25px] rounded-full" src={Profile} alt="" />
-                            <Button onClick={() => navigate("/login")} content={"Login"} className={"mx-3 text-white bg-transparant hover:bg-slate-500 py-2 px-3 rounded-lg"} type={"button"} />
-                            <Button onClick={() => navigate("/register")} content={"Daftar"} className={"text-white bg-transparant hover:bg-slate-500 py-2 px-3 rounded-lg"} type={"button"} />
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-=======
   const navigate = useNavigate();
   const token = (localStorage.getItem("token") as string) || null;
 
@@ -124,7 +76,7 @@ function Navbar() {
                 type={"button"}
               />
             ) : (
-              <p className="mx-3">Timothy Subekti</p>
+              <p className="mx-3">{localStorage.getItem("fullName")}</p>
             )}
             {!token ? (
               <Button
@@ -150,7 +102,6 @@ function Navbar() {
       </div>
     </section>
   );
->>>>>>> 05235748255ba243a3e84c598dd9b91836cdcae5
 }
 
 export default Navbar;
